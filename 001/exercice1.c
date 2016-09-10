c0aa1348f5ff1994afb2c026f2749226918a484c
Administrator <root@lezarius.eu>
2016-09-10 12:11:38 +0200

    Exercise One

---
+++ 001/exercice1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+  puts ("bonjours Lezarius belle journée pour apprendre =D Un appuie sur une simple touch me fera très plaisir <3");
+  getchar ();/* waiting put enter */
+  puts ("Je prend cette action pour un oui bonne continuation <3 *DataLove*"); 
+  return 0;
+}
---
+++ 001/programe1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+  puts ("bonjours Lezarius belle journée pour apprendre =D");
+  getchar ();/* waiting put enter */
+  
+  return 0;
+}
